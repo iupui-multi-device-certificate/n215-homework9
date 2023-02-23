1bf7675419efd441cee7d7da3d142c382f0bb24a
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-23 16:45:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+##N215
+
+### Homework Seven
+
+https://in-info-web4.informatics.iupui.edu/~rahensle/n215/homework-eight
---
+++ app/app.js
@@ -0,0 +1,16 @@
+// import { homeContent } from "../app/views/home.js";
+// import { adoptContent } from "../app/views/adopt.js";
+// import { blogContent } from "../app/views/blog.js";
+// import { aboutContent } from "../app/views/about.js";
+
+function initListeners() {
+  $("nav a").click(function (e) {
+    // let aID = e.currentTarget.id;
+    // let contentID = aID + "Content";
+    // $("#app").html(eval(contentID));
+  });
+}
+$(document).ready(function () {
+  // initListeners();
+  // $("#app").html(homeContent);
+});
