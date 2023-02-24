c9d77a4938c7f2f2568dc3e35fde3913375f1765
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-24 12:06:45 -0500

    refactor(models): aggregate into model.js file

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
-import { homeModel } from "../app/models/homeModel.js";
 import { homeView } from "../app/views/homeView.js";
 
-import { adoptModel } from "../app/models/adoptModel.js";
 import { adoptView } from "../app/views/adoptView.js";
 
-import { blogModel } from "../app/models/blogModel.js";
 import { blogView } from "../app/views/blogView.js";
 
-import { aboutModel } from "../app/models/aboutModel.js";
 import { aboutView } from "../app/views/aboutView.js";
 
+import {
+  homeModel,
+  adoptModel,
+  blogModel,
+  aboutModel,
+} from "./models/model.js";
 const homeContent = homeView(homeModel);
 const blogContent = blogView(blogModel);
 const adoptContent = adoptView(adoptModel);
