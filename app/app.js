c0588727e76e38406d68e584b1268d27e7baac4b
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-24 19:55:24 -0500

    feature: dynamic page title

--- app/app.js
+++ app/app.js
@@ -13,12 +13,19 @@ const blogContent = blogView(blogModel);
 const adoptContent = adoptView(adoptModel);
 const aboutContent = aboutView(aboutModel);
 
+const titleBase = "Paw Savers";
 const changeContent = (contentID) => $("#app").html(eval(contentID));
-const setHome = () => $("#app").html(homeContent);
+const setHome = () => {
+  $("#app").html(homeContent);
+  $(document).attr("title", `${titleBase} | HOME`);
+};
 
 function initListeners() {
   $("nav a").click(function (e) {
     let aID = e.currentTarget.id;
+
+    $(document).attr("title", `Paw Savers | ${aID.toUpperCase()}`);
+
     let contentID = aID + "Content";
     changeContent(contentID);
   });
